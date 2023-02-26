491e9d84997ad4574e4ef1a1b8201e516a7f94b4
Francisco Dadone <dadonefran@gmail.com>
2023-02-26 13:58:31 -0300

    Mensajes en la pestaña del ticket

--- web/resources/views/ticket.blade.php
+++ web/resources/views/ticket.blade.php
@@ -9,12 +9,20 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    ssdfsfdsd
+    <div style="display: flex; height: 100%;">
+        <div style="width: 20%;" class="py-2 px-2 my-2 mx-2 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            adsa
+        </div>
+        <div class="py-2 bg-gray-400" style="width: 80%; height: 100%">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8" style="height: 100%">
+                <h1 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight py-3">
+                    Mensajes
+                </h1>
+                @foreach ($ticket->messages() as $message)
+                <div class="my-1 p-6 text-gray-900 dark:text-gray-100 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    {{ $message->message }}
                 </div>
+                @endforeach
             </div>
         </div>
     </div>
