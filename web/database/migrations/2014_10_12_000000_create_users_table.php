3dbf8487be7d1cb123b4f02693143f11077def6c
Francisco Dadone <dadonefran@gmail.com>
2023-02-26 11:30:58 -0300

    Corrección vite

--- web/database/migrations/2014_10_12_000000_create_users_table.php
+++ web/database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use App\Models\User;
 return new class extends Migration
 {
     /**
@@ -22,6 +22,12 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin')
+        ]);
     }
 
     /**
--- web/vite.config.js
+++ web/vite.config.js
@@ -5,10 +5,15 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
+                'resources/css/app.css',
                 'resources/js/app.js',
             ],
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+        open: false,
+    }
 });
